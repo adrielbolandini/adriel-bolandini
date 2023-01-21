fa1b5ceadf371c6b12b0b84142b4afdf3fb0bfd2
Adriel Bolandini <bolandiniadriel@gmail.com>
2023-01-21 18:47:54 -0300

    Incluir navegação to Home

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,13 +1,14 @@
-import * as tailwindcss from 'tailwindcss'; 
+import { redirect } from "next/dist/server/api-utils"
+import Router from "next/router"
 
 export default function Header(){
     return(
-        <div className="flex flex-row h-1/6 w-screen ">
+        <div className="flex flex-row h-16 w-screen ">
             <img  className='m-6 absolute w-32 resize rounded-full' src='https://avatars.githubusercontent.com/u/64433279?v=4'/>
-            <ul className='w-full h-full my-28 flex flex-row text-white items-center bg-blue-600'>
-                <li className='text-center basis-1/3 '>Inicio</li>
-                <li className='text-center basis-1/3 '>Projetos</li>
-                <li className='text-center basis-1/3 '>LinkedIn</li>
+            <ul className='w-full h-full pr-32 pl-32 my-28 flex flex-row text-white justify-between text-center items-center bg-blue-600'>
+                <li className='basis-1/3 hover:bg-cyan-400 hover:h-full' onClick={()=>Router.push('/')}>Adriel Bolandini</li>
+                <li className='basis-1/3 hover:bg-cyan-400 hover:h-full'>Dev</li>
+                <li className='basis-1/3 hover:bg-cyan-400 hover:h-full' onClick={()=>Router.replace('www.google.com')}>LinkedIn</li>
             </ul>
         </div>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import Header from '../components/Header'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+
   return (
     <div className='bg-black w-screen h-screen'>
       <Head>
